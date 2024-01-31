b02e0bc8edff498d7c2b34bf5964c449ea5e9199
FranciscoMM01 <154660290+FranciscoMM01@users.noreply.github.com>
2024-01-31 16:20:43 -0600

    Update Acomodador.py
    Se actualiza el archivo añadiendo soporte para archivos comprimidos, .pdf, .doc, .Xml y ,exe

--- Python/Acomodador.py
+++ Python/Acomodador.py
@@ -3,12 +3,16 @@ import os
 import shutil
             # En las rutas puse ejemplos con el prefijo r para tratar las rutas como cadenas crudas 
             # para evitar tener problemas con python. En otro caso puede usarse las diagonales /
-downloadsFolder = "C:/Users/frank/Downloads"
+downloadsFolder = r"C:/Users/frank/Downloads"
 Picturefolder = r"C:\Users\frank\Downloads\Pictures" 
 videoFolder = r"C:\Users\frank\Downloads\Video"
-DocsFolder = "C:/Users/frank/Downloads/Docs"
-OsuFolder = r"C:\Users\frank\Downloads\Osu_Songs"
-
+DocsFolder = r"C:/Users/frank/Downloads/Docs"
+OsuFolder = r"C:\Users\frank\Osu_Songs"
+ZipFolder = r"C:\Users\frank\Documents\ZIP_RAR"
+XMLFolder = r"C:\Users\frank\Documents\XML"
+XLSDOCSFolder = r"C:\Users\frank\Documents\XLS_DOC"
+ExeFolder = r"C:\Users\frank\Downloads\Exes"
+        
 if __name__ == "__main__":
     for filename in os.listdir(downloadsFolder):
         #* Obtenemos la ruta completa del archivo
@@ -45,3 +49,18 @@ if __name__ == "__main__":
             os.rename(full_path, os.path.join(OsuFolder, filename))
             print(name + ": " + extension)
 
+        elif extension.lower() in [".rar", ".zip", ".7z"]:
+            os.rename(full_path, os.path.join(ZipFolder, filename))
+            print(name +": " + extension)
+
+        elif extension.lower() == ".xml":
+            os.rename(full_path, os.path.join(XMLFolder, filename))
+            print(name +": " + extension)
+
+        elif extension.lower() in [".xls", ".docx", ".xlsx", ".xlsm"]:
+            os.rename(full_path, os.path.join(XLSDOCSFolder, filename))
+            print(name +": " + extension)
+
+        elif extension.lower() in [".exe", ".msi"]:
+            os.rename(full_path, os.path.join(ExeFolder, filename))
+            print(name +": " + extension)
