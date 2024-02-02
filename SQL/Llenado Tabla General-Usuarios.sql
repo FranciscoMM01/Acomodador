4989ab560c03e8461a19c65efe7ef44dba2795e9
FranciscoMM01 <154660290+FranciscoMM01@users.noreply.github.com>
2024-02-01 23:51:43 -0600

    Update Llenado Tabla General-Usuarios.sql
    Se comenta fragmentos del codigo

--- SQL/Llenado Tabla General-Usuarios.sql
+++ SQL/Llenado Tabla General-Usuarios.sql
@@ -1,3 +1,4 @@
+-- Declaracion de variables para ciclos y constantes
 DECLARE @i INT = 1
 DECLARE @name VARCHAR(30)
 DECLARE @apell VARCHAR(30)
@@ -8,24 +9,25 @@ WHILE @i <= (SELECT MAX(ID) FROM dbo.Names) -- WHILE 1
 DECLARE @Fecha DATE = DATEFROMPARTS(1900 + (@i * 30), @i * 2, @i * 8);
 BEGIN
 
-	--Obtener el valor del nombre en la posición @i
+	--Obtener el valor del nombre en la posiciÃ³n @i
 	SELECT @name = nombre FROM dbo.Names WHERE ID = @i;
 	SELECT @apell = apellido FROM dbo.Names WHERE ID = @i
 	
 	-- Insertar dentro de la tabla General.Usuario
-	INSERT INTO General.Usuarios(Tipo, Correo, Nombre, Apellido, NombreUsuario, Edad, Cumpleaños, Ciudad, Contraseña)
-	VALUES(1, 
-		LEFT(@name,3)+'@'+LEFT(@apell,3)+'.com', 
-		@name,
-		@apell, 
-		LEFT(@name,3)+LEFT(@apell, 3)+ CONVERT(VARCHAR(3),@i),
-		@i*20-0.5*@i, 
-		@Fecha, 
-		'CIUDAD'+CONVERT(VARCHAR(3),@i), 
-		@i*123 
+	INSERT INTO General.Usuarios(Tipo, Correo, Nombre, Apellido, NombreUsuario, Edad, CumpleaÃ±os, Ciudad, ContraseÃ±a)
+	VALUES(1, --TIPO
+		LEFT(@name,3)+'@'+LEFT(@apell,3)+'.com', --CORREO
+		@name, --NOMBRE
+		@apell,  -- APELLIDO
+		LEFT(@name,3)+LEFT(@apell, 3)+ CONVERT(VARCHAR(3),@i), --NOMBRE USUARIO
+		@i*20-0.5*@i, --EDAD
+		@Fecha, --FECHA
+		'CIUDAD'+CONVERT(VARCHAR(3),@i),  --CIUDAD
+		@i*123 --CONTRASEÃ‘A
 		)
 
 	SET @i = @i +1 -- Incrementar WHILE 1
 END -- Fin WHILE 1
 
+		
 SELECT * FROM general.Usuarios
