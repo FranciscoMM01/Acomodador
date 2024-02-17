7ff4e78f36e7c8354471d922935fc63fe6668c5f
FranciscoMM01 <154660290+FranciscoMM01@users.noreply.github.com>
2024-02-16 23:05:11 -0600

    Update Reacomodar Folios Factura.sql
    Se generan los querys para la primera tabla a corregir.
    Ejecutándose, sin embargo en forma de print para no afectar a la base de pruebas.

--- SQL/HerramientasTrabajo/Reacomodar Folios Factura.sql
+++ SQL/HerramientasTrabajo/Reacomodar Folios Factura.sql
@@ -37,6 +37,29 @@ SELECT * FROM #FiltroFolios
 
 --DECLARE @FolioSig INT = MAX(SELECT Folios TOP(1) FROM #FiltroFolios)
 DECLARE @FolioSig INT  = (SELECT TOP(1) FOLIOS from #FiltroFolios order by folios desc) + 1;
-PRINT @FolioSig
+PRINT ('El próximo folio es : '+ CAST(@FolioSig AS VARCHAR(20)))
+
+
+					--COMENZAR CON LA CORRECION
+DECLARE @i INT = 1
+DECLARE @j INT = 1
+DECLARE @TotalFilas INT
+
+SELECT @TotalFilas = COUNT(*) FROM Folios
+
+WHILE @i <= @TotalFilas		--Obtener Folios viejos uno por uno
+BEGIN
+	DECLARE @ValorColumna INT;
+	SELECT @ValorColumna = FolioViejo
+	FROM Folios
+	WHERE ID = @i
+
+	--PRINT @ValorColumna
+
+		--print('VIEJO: ' + CAST(@ValorColumna AS VARCHAR(10)) + ' NUEVO: ' + CAST(@folioSig AS VARCHAR(10)))
+		PRINT('UPDATE VE_Factura SET FolioFac = '+ CAST(@folioSig AS VARCHAR(10)) + ' WHERE FolioFac = ' + CAST(@ValorColumna AS VARCHAR(10)) + ' AND SerieFac = ''MM''')
+		SET @i = @i + 1;
+		SET @FolioSig = @FolioSig +1
+END
 DROP TABLE #FiltroFolios
 
